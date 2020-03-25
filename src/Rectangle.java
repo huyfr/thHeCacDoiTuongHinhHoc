f82cafa11f41ce1a40b7b40558157116710136d2
huyfr <61008601+huyfr@users.noreply.github.com>
2020-03-25 22:05:38 +0700

    Thực hành hệ các đối tượng hình học

---
+++ src/Rectangle.java
@@ -0,0 +1,52 @@
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double height = 1.0;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double height) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "A rectangle with width = " + getWidth() + " and length = " + getHeight() + super.toString();
+    }
+}
+
+class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle("orange", true, 2.3, 5.8);
+        System.out.println(rectangle);
+    }
+}
